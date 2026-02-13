abf3bdffeaf40223137af9ae1d8176def2511d73
DavidMoreno-ASIR <159730059+DavidMoreno170622@users.noreply.github.com>
2026-02-13 11:20:12 +0100

    Update info.php

--- php/POO/info.php
+++ php/POO/info.php
@@ -1,8 +1,8 @@
 <?php
-$host = "localhost";
-$db   = "_ejemplo_";
-$user = "david";
-$pass = "CarmenCM_8913!";
+$host = "mysql";              // 👈 CLAVE
+$db   = "crud_db";            // debe coincidir con MYSQL_DATABASE
+$user = "root";               // o el usuario real
+$pass = "root";               // password del docker-compose
 $charset = "utf8mb4";
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
